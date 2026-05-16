bf01f9eb2d1cd361c844a93bf09755a3b0d5b9fa
ATHUL KRISHNA P S <athulkrishnaps3124@gmail.com>
2026-05-16 17:07:44 +0530

    fix: make ex-8.c a true (minimal change) Go-Back-N simulation; variable style kept close to original.

--- ex-8.c
+++ ex-8.c
@@ -10,6 +10,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#define WINDOW_SIZE 4
+#define MAX_PACKET 9
+
 void itoa(int number, char numberString[])
 {
     numberString[0] = (char)(number + 48);
@@ -18,15 +21,14 @@ void itoa(int number, char numberString[])
 
 int main()
 {
-    int sockfd, new_sockfd, windowStart = 1, windowCurrent = 1, windowEnd = 4;
-    int oldWindowStart, flag;
+    int sockfd, new_sockfd, windowStart = 1, windowEnd = WINDOW_SIZE, windowCurrent;
     char buffer[100];
     socklen_t len;
-
     struct sockaddr_in server, client;
+    int all_sent = 0;
 
     server.sin_family = AF_INET;
-    server.sin_port = 3033;
+    server.sin_port = htons(3033);
     server.sin_addr.s_addr = INADDR_ANY;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -41,48 +43,43 @@ int main()
 
     len = sizeof(client);
     listen(sockfd, 1);
-
     new_sockfd = accept(sockfd, (struct sockaddr *)&client, &len);
-
     recv(new_sockfd, buffer, 100, 0);
+    printf("\nReceived a request from client. Sending packets (Go-Back-N)...");
 
-    fcntl(new_sockfd, F_SETFL, O_NONBLOCK);
-
-    printf("\nReceived a request from client. Sending packets one by one...");
-
-    do
+    while (!all_sent)
     {
-        if (windowCurrent != windowEnd)
+        // Send packets in current window
+        for (windowCurrent = windowStart; windowCurrent <= windowEnd && windowCurrent <= MAX_PACKET; windowCurrent++)
         {
             itoa(windowCurrent, buffer);
             send(new_sockfd, buffer, 100, 0);
-
-            printf("\nPacket Sent: %d\n", windowCurrent);
-            windowCurrent++;
+            printf("\nPacket Sent: %d", windowCurrent);
         }
 
+        // Wait for ACK or RETRANSMIT
         recv(new_sockfd, buffer, 100, 0);
-
         if (buffer[0] == 'R')
         {
-            printf("\n** Received a RETRANSMIT packet. Resending packet...");
-            send(new_sockfd, buffer, 100, 0);
-
-            windowCurrent = atoi(&buffer[1]);
+            int pkt = atoi(&buffer[1]);
+            printf("\n** Received a RETRANSMIT for packet %d. Resending window from %d...", pkt, pkt);
+            windowStart = pkt; // Go back
+            windowEnd = windowStart + WINDOW_SIZE - 1;
         }
-
-        if (buffer[0] == 'A')
+        else if (buffer[0] == 'A')
         {
-            windowStart = windowStart + (windowStart - oldWindowStart);
+            int ack = atoi(&buffer[1]);
+            printf("\n** Received ACK for packet %d.", ack);
+            windowStart = ack + 1;
+            windowEnd = windowStart + WINDOW_SIZE - 1;
+            if (ack >= MAX_PACKET)
+                all_sent = 1;
         }
+    }
 
-    } while (windowCurrent != windowEnd);
-
-    printf("\nAll packets sent successfully. Closing connection.\n");
-
+    printf("\nAll packets sent and acknowledged. Closing connection.\n");
     close(new_sockfd);
     close(sockfd);
-
     return 0;
 }
 
@@ -96,74 +93,57 @@ int main()
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define WINDOW_SIZE 4
+#define MAX_PACKET 9
+
 int main()
 {
-    int sockfd, firstTime = 1, currentPacket, wait = 3;
+    int sockfd, firstTime = 1, currentPacket;
     char buffer[100], digit[2];
-
     struct sockaddr_in server;
-
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
     server.sin_family = AF_INET;
-    server.sin_port = 3033;
+    server.sin_port = htons(3033);
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
-
     printf("\nStarting up...");
     int len = sizeof(server);
-
     printf("\nEstablishing connection to server...");
     connect(sockfd, (struct sockaddr *)&server, len);
-
     sprintf(buffer, "REQUEST");
     send(sockfd, buffer, strlen(buffer), 0);
-
-    do
+    int expected = 1;
+    int all_received = 0;
+    while (!all_received)
     {
+        memset(buffer, 0, sizeof(buffer));
         recv(sockfd, buffer, 100, 0);
-
         currentPacket = atoi(buffer);
-
         printf("\nGot packet: %d", currentPacket);
-
         if (currentPacket == 3 && firstTime)
         {
             printf("\n** Simulation: Packet data corrupted or incomplete.");
-            printf("\n** Sending RETRANSMIT for packet 1.");
-
-            memset(buffer, 0, sizeof(buffer));
-            sprintf(buffer, "R1");
-
+            printf("\n** Sending RETRANSMIT for packet 3.");
+            sprintf(buffer, "R3");
             send(sockfd, buffer, strlen(buffer), 0);
-
             firstTime = 0;
+        }
+        else if (currentPacket == expected)
+        {
+            printf("\n** Packet Accepted -> Sending ACK\n");
+            sprintf(buffer, "A%d",currentPacket);
+            send(sockfd, buffer, strlen(buffer), 0);
+            expected++;
+            if (currentPacket >= MAX_PACKET)
+                all_received = 1;
         }
         else
         {
-            wait--;
-
-            if (!wait)
-            {
-                printf("\n** Packet Accepted -> Sending ACK\n");
-
-                wait = 3;
-
-                sprintf(buffer, "A");
-
-                digit[0] = (char)(currentPacket + 48);
-                digit[1] = '\0';
-
-                strcat(buffer, digit);
-
-                send(sockfd, buffer, strlen(buffer), 0);
-            }
+            // Out of order or duplicate, send ACK for last accepted
+            sprintf(buffer, "A%d", expected-1);
+            send(sockfd, buffer, strlen(buffer), 0);
         }
-
-    } while (currentPacket != 9);
-
+    }
     printf("\nAll packets received. Exiting.\n");
-
     close(sockfd);
-
     return 0;
 }
